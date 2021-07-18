08d897ef6646132b6e46caf509ff4a6987e34eac
AoifeCarty <agcdunne@gmail.com>
2021-07-18 11:12:33 +0100

    Removed unusable rows

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+data=pd.read_csv("Popular Movies TV shows from Prime Videos Netflix version_3.csv")
+print(data.shape)
+print(data.isna())
+print(data.isna().sum())
+newdata=data.dropna()
+print(newdata.isna().sum())
+print(newdata.info())
+
+#Remove duplicate titles
+ND2=newdata.sort_values('Title', ascending=True)
+ND2=newdata.drop_duplicates(subset='Title')
+print(ND2)
