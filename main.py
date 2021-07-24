05d91435f3b6b318a333e147706e7c259187eabd
AoifeCarty <agcdunne@gmail.com>
2021-07-24 11:47:22 +0100

    Final Code

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import seaborn as sns
 import requests
 import openpyxl
 
-
-
 data=pd.read_csv("Popular Movies TV shows from Prime Videos Netflix version_3.csv")
 print(data.shape)
 print(data.isna())
@@ -19,7 +17,6 @@ print(newdata.info())
 ND2=newdata.sort_values('Title', ascending=True)
 ND2=newdata.drop_duplicates(subset='Title')
 print(ND2)
-
 my_array=np.array(ND2)
 print(my_array)
 
@@ -50,6 +47,11 @@ plt.ylabel('No. of Movies')
 plt.title('Number of Movies by Age Rating')
 plt.show()
 
+sns.scatterplot(x='IMDb',
+                y='Rotten Tomatoes',
+                data=finaldata)
+plt.show()
+
 #Selecting Top 10
 Top10TS=(finaldata.sort_values(by=['TotalScore'],ascending=False).head(10))
 
@@ -57,6 +59,15 @@ Top10IMDb=(finaldata.sort_values(by=['IMDb'],ascending=False).head(10))
 print(Top10IMDb)
 Top10RT=(finaldata.sort_values(by=['Rotten Tomatoes'],ascending=False).head(10))
 print(Top10RT)
-
 Top10IMDb.to_excel(r'C:\Users\aoife\exportIMDb.xlsx', index=False, header=True)
 Top10RT.to_excel(r'C:\Users\aoife\exportRT.xlsx', index=False, header=True)
+#API
+url = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol=amzn&apikey=QA2KXC8ZF2YYNVEV'
+r = requests.get(url)
+company_overview = r.json()
+print(company_overview)
+#Adding Second data set and merge
+data2=pd.read_csv('Prime TV Shows.csv')
+Mergedata=pd.merge(data2, finaldata, left_index=True, right_index=True)
+print(Mergedata)
+print(Mergedata.info())
