032b70fea69087a56e596fb71edcac08d495d55a
AoifeCarty <agcdunne@gmail.com>
2021-07-24 10:52:21 +0100

    Analytics and Charts

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import requests
+import openpyxl
+
+
 
 data=pd.read_csv("Popular Movies TV shows from Prime Videos Netflix version_3.csv")
 print(data.shape)
@@ -15,3 +19,44 @@ print(newdata.info())
 ND2=newdata.sort_values('Title', ascending=True)
 ND2=newdata.drop_duplicates(subset='Title')
 print(ND2)
+
+my_array=np.array(ND2)
+print(my_array)
+
+#Removed Unused Columns and add Total Column
+finaldata=ND2.drop(['Unnamed: 0','Netflix','Amazon Prime Video'],axis=1)
+finaldata.IMDb=finaldata.IMDb.astype(float)
+finaldata['Rotten Tomatoes'] = finaldata['Rotten Tomatoes'].astype(float)
+print(finaldata.info())
+TotalScore=finaldata['IMDb']+finaldata['Rotten Tomatoes']
+finaldata['TotalScore']=TotalScore
+print(finaldata)
+
+#Analytics & Charts
+print(finaldata['TotalScore'].max())
+print(finaldata['TotalScore'].min())
+print(finaldata.groupby('Rating').first())
+
+#Bar Chart No. of Movies
+def addlabels(x,y):
+    for i in range(len(x)):
+        plt.text(i,y[i],y[i])
+x=['18+','16+','13+','7+','All']
+y=finaldata['Rating'].value_counts()
+addlabels(x,y)
+plt.bar(x,y, color='hotpink')
+plt.xlabel('Rating')
+plt.ylabel('No. of Movies')
+plt.title('Number of Movies by Age Rating')
+plt.show()
+
+#Selecting Top 10
+Top10TS=(finaldata.sort_values(by=['TotalScore'],ascending=False).head(10))
+
+Top10IMDb=(finaldata.sort_values(by=['IMDb'],ascending=False).head(10))
+print(Top10IMDb)
+Top10RT=(finaldata.sort_values(by=['Rotten Tomatoes'],ascending=False).head(10))
+print(Top10RT)
+
+Top10IMDb.to_excel(r'C:\Users\aoife\exportIMDb.xlsx', index=False, header=True)
+Top10RT.to_excel(r'C:\Users\aoife\exportRT.xlsx', index=False, header=True)
